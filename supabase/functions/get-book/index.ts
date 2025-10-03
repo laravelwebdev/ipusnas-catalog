1afebb31b762d50d13dc2b3bb9309f8dcb67e4c6
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-10-03 22:05:20 +0800

    Update index.ts

--- supabase/functions/get-book/index.ts
+++ supabase/functions/get-book/index.ts
@@ -1,59 +1,58 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
-
 import * as QueryString from "querystring";
 import { CLIENT_ID, BASE_URL, USER_AGENT, USER_ID } from "../_shared/consts.ts";
-import {
-  decrypt,
-  resolvePassKey,
-  resolvePassZip,
-  resolvePassPdf,
-} from "../_shared/decrpt.ts";
+import { decrypt, resolvePassKey, resolvePassZip, resolvePassPdf } from "../_shared/decrpt.ts";
 import { corsHeaders } from "../_shared/cors.ts";
-import type { BookItem } from "../_shared/interfaces.ts";
-
-async function checkProfile(query: QueryString.ParsedQuery) {
-  const accessToken = <string>query.access_token;
+async function checkProfile(query) {
+  const accessToken = query.access_token;
   const queryParams = new URLSearchParams([
-    ["client_id", CLIENT_ID],
+    [
+      "client_id",
+      CLIENT_ID
+    ]
   ]);
   if (accessToken) {
     queryParams.append("access_token", accessToken);
   }
-  const base = `${BASE_URL}/profile`;
+  const base = `${BASE_URL}/webhook/profile`;
   const reqUrl = `${base}?${queryParams.toString()}`;
-
   return await fetch(reqUrl, {
-    headers: { "user-agent": USER_AGENT },
-  })
-    .then((r) => r.json())
-    .then((d) => d?.data);
+    headers: {
+      "user-agent": USER_AGENT
+    }
+  }).then((r)=>r.json()).then((d)=>d?.data);
 }
-
-async function getBook(query: QueryString.ParsedQuery): Promise<BookItem> {
-  const bookId = <string>query.book_id;
-  const accessToken = <string>query.access_token;
+async function getBook(query) {
+  const bookId = query.book_id;
+  const accessToken = query.access_token;
   const queryParams = new URLSearchParams([
-    ["client_id", CLIENT_ID],
-    ["book_id", bookId],
+    [
+      "client_id",
+      CLIENT_ID
+    ],
+    [
+      "book_id",
+      bookId
+    ]
   ]);
   if (accessToken) {
     queryParams.append("access_token", accessToken);
   }
-  const base = `${BASE_URL}/books/detail`;
+  const base = `${BASE_URL}/webhook/book-detail`;
   const reqUrl = `${base}?${queryParams.toString()}`;
-
   return await fetch(reqUrl, {
-    headers: { "user-agent": USER_AGENT },
-  })
-    .then((r) => r.json())
-    .then((d) => d?.data);
+    headers: {
+      "user-agent": USER_AGENT
+    }
+  }).then((r)=>r.json()).then((d)=>d?.data);
 }
-
-Deno.serve(async (req) => {
+Deno.serve(async (req)=>{
   if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
+    return new Response("ok", {
+      headers: corsHeaders
+    });
   }
   const query = QueryString.parse(new URL(req.url).search);
   const data = await getBook(query);
@@ -66,6 +65,9 @@ Deno.serve(async (req) => {
     data.Item.pass_pdf = await resolvePassPdf(decrypted);
   }
   return new Response(JSON.stringify(data), {
-    headers: { ...corsHeaders, "Content-Type": "application/json" },
+    headers: {
+      ...corsHeaders,
+      "Content-Type": "application/json"
+    }
   });
 });
